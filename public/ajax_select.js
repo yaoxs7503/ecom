374fac9f2fdc3a86b7ece0f4ec1c31e426e79e1d
philip_windows10 <348180212@qq.com>
2019-12-13 17:40:44 +0800

    修改异步传输的过程

--- public/ajax_select.js
+++ public/ajax_select.js
@@ -1,6 +1,4 @@
-var xmlHTTP;
 // xmlHTTP=new XMLHttpRequest();
-
 function $_XMLHttpRequest(){
   if(window.ActiveXObject){
     xmlHTTP=new ActiveXObject("Microsoft.XMLHTTP");
@@ -12,15 +10,14 @@ function $_XMLHttpRequest(){
 } 
 
 function check(){
-  const select_op=document.getElementById("select_op").Value;
-  $_XMLHttpRequest();
-  xmlHTTP.open("GET","jqajax_check.php?select_op="+select_op,true);
+  let select_op=document.getElementById("select_op").value;
+  console.log(select_op);
+  xmlHTTP=new XMLHttpRequest();
+  xmlHTTP.open("POST","jqajax_check.php?select_op="+select_op,true);
   xmlHTTP.onreadystatechange=function check_user(){
     if(xmlHTTP.readyState==4){
-      if(xmlHTTP.status==200){
         const str=xmlHTTP.responseText;
         document.getElementById("message").innerHTML=str;
-      } 
     }
   }
   xmlHTTP.send(null);
--- public/jqajax_check.php
+++ public/jqajax_check.php
@@ -1,5 +1,7 @@
 <?php
+
   $select_op=$_GET['select_op'];
+  if(isset($select_op)){
   if($select_op!==""){
     if($select_op=="1"){
       echo "你选择到1的选项";
@@ -11,3 +13,6 @@
   }else{
     echo "请选择一个选项";
   }
+}else{
+  echo "没有传到数据";
+}
